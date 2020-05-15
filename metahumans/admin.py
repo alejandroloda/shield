dd38a5b4349f0ddf1d8041f2856678e70ae6cc56
alejandroloda <alejandroloda@gmail.com>
2020-05-15 19:11:09 +0100

    inicial

---
+++ metahumans/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from . import models
+
+
+# Register your models here.
+
+class PowerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'level')
+
+
+admin.site.register(models.Power, PowerAdmin)
+
+
+class MetaHumanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'active', 'level', 'ally')
+    list_filter = ('active', 'level', 'ally')
+
+
+admin.site.register(models.MetaHuman, MetaHumanAdmin)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'headquarter')
+    list_filter = (['headquarter'])
+
+
+admin.site.register(models.Team, TeamAdmin)
